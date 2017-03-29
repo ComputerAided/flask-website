915614a38df7df205bb0fc1d41d36f3a97f60531
Kenny H <ComputerAided@users.noreply.github.com>
2017-03-28 22:24:22 -0600

    Add new files

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("main.html")
+
+if __name__ == '__main__':
+    app.run(host = '10.0.0.17', port = 80, debug = True)
