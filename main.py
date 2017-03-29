d651ada21ab311c8c879cd10b8139cb88d9f10c6
Kenny H <ComputerAided@users.noreply.github.com>
2017-03-29 11:35:35 -0600

    Changes to style

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ def main():
     return render_template("main.html")
 
 if __name__ == '__main__':
-    app.run(host = '127.0.0.1', port = 80, debug = True) # Feel free to change to your IP
+    app.run(host = '10.0.0.17', port = 80, debug = True) # Feel free to change to your IP
